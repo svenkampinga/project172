6005d7a15ca372b9aa267a49705168c9ef617854
gromdroid <gromdroid@gmail.com>
2017-11-10 12:32:54 +0100

    Update scheduler

--- AVR_TTC_scheduler.c
+++ AVR_TTC_scheduler.c
@@ -1,24 +1,19 @@
 /*
- * GccApplication1.c
+ * AVR_TTC_scheduler.c
  *
- * Created: 2-11-2017 11:45:50
- *  Author: gaauwe
+ * Created: 10-11-2017
+ *  Author: Gaauwe en Steven
  */ 
-//tx serial
 
 #include <avr/io.h> 
 #include <util/delay.h>
 #include "AVR_TTC_scheduler.h"
 #include <serial_OUT.h>
 #include <serial_IN.h>
-
 #include <stdio.h>
-
-
 #include <uart.h>
 
 
-#include <avr/interrupt.h>
 
 // The array of tasks
 sTask SCH_tasks_G[SCH_MAX_TASKS];
@@ -35,9 +30,7 @@ sTask SCH_tasks_G[SCH_MAX_TASKS];
 
 
 
-#define BUAD_RATE_CALC ((F_CPU/16/BUAD) - 1)  
-#define buffer_size 128
-#define boolen 0
+
 
 void SCH_Dispatch_Tasks(void)
 {
@@ -249,10 +242,6 @@ void ledTest(){
 	PORTB |= _BV(1);
 	PORTB |= _BV(3);
 	_delay_ms(5000);
-
-	
-	
-	
 }	
 
 void ledRood(){
@@ -263,6 +252,38 @@ void ledRood(){
 }	
 
 void ledBlauw(){
+	PORTB &= ~_BV(1);
+	PORTB &= ~_BV(2);
+	PORTB |= _BV(3);
+	_delay_ms(10000);
+	PORTB &= _BV(1);
+	PORTB &= ~_BV(2);
+	PORTB &= ~_BV(3);
+	_delay_ms(10000);
+	PORTB &= ~_BV(1);
+	PORTB &= ~_BV(2);
+	PORTB |= _BV(3);
+	_delay_ms(10000);
+	PORTB &= _BV(1);
+	PORTB &= ~_BV(2);
+	PORTB &= ~_BV(3);
+	_delay_ms(10000);
+	PORTB &= ~_BV(1);
+	PORTB &= ~_BV(2);
+	PORTB |= _BV(3);
+	_delay_ms(10000);
+	PORTB &= _BV(1);
+	PORTB &= ~_BV(2);
+	PORTB &= ~_BV(3);
+	_delay_ms(10000);
+	PORTB &= ~_BV(1);
+	PORTB &= ~_BV(2);
+	PORTB |= _BV(3);
+	_delay_ms(10000);
+	PORTB &= _BV(1);
+	PORTB &= ~_BV(2);
+	PORTB &= ~_BV(3);
+	_delay_ms(10000);
 	PORTB &= ~_BV(1);
 	PORTB &= ~_BV(2);
 	PORTB |= _BV(3);
@@ -301,38 +322,31 @@ void read_serial(){
 	char input;
 	input = getchar();
 	sprintf(input2, "%c", input);
-	//puts(input2);
 	
 	if (strchr(input2, '0') != NULL)
 	{
-		ledRood();
-	} else if (strchr(input2, '1') != NULL){
-		ledBlauw();
-	} else if (strchr(input2, '2') != NULL){
 		ledGroen();
-	}	
+	} else if (strchr(input2, '1') != NULL){
+		ledRood();
+	} else if (strchr(input2, '2') != NULL){
+		ledBlauw();
+		ledRood();
+	} else if (strchr(input2, '3') != NULL){
+		ledBlauw();
+		ledGroen();
+	}
 }	
 			
 void read_light_sensor(){
 	int reading = ReadADC(0);
-	
-	/*
-	char reader[50];
-	sprintf(reader, "%i", reading);
-	
-	serialWrite("|L");
-	serialWrite(reader);
-	serialWrite("|\n");
-	*/
 	char reader[50];
 	sprintf(reader, "%i", reading);
 
-	//strcat(reader, " C");
 	puts(reader);
 }
 
 
-void gettemp(){
+void read_temp(){
 	ADMUX &= ~_BV(MUX0); // Set channel point to port 0
 	ADCSRA |= _BV(ADSC); // Start adc measurement
 	loop_until_bit_is_clear(ADCSRA, ADSC); // proceed when done
@@ -341,28 +355,8 @@ void gettemp(){
 	
 	char reader[50];
 	sprintf(reader, "%i", temp);
-
-	//strcat(reader, " C");
+	
 	puts(reader);
-}
-
-void getdistance(){
-	DDRB = 0x00;
-	PORTB |= _BV(PB0);
-	_delay_us(10);
-	PORTB &= ~_BV(PB0);
-
-	loop_until_bit_is_set(PINB, PB1);
-	TCNT1 = 0;
-	loop_until_bit_is_clear(PINB, PB1);
-	unsigned int count = TCNT1;
-	
-	int distance = ((int)count / 16) / 58;
-
-	//char reader[50];
-	//sprintf(reader, "%i", distance);
-	
-	send_serial("hello");
 }
 
 void uart_init(void) {
@@ -391,41 +385,6 @@ char uart_getchar(FILE *stream) {
 	loop_until_bit_is_set(UCSR0A, RXC0);
 	return UDR0;
 }
-			
-void send_serial(char text[]){
-				char br;
-                char ar[]= "Hello";
-                char buffer[10]; 
-                //High and low bits
-                UBRR0H = (BUAD_RATE_CALC >> 8); 
-                UBRR0L = BUAD_RATE_CALC; 
-                //////////////// 
-                //transimit and recieve enable
-                UCSR0B = (1 << TXEN0)| (1 << TXCIE0) | (1 << RXEN0) | (1 << RXCIE0); 
-                UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);  //8 bit data format
-                ////////////////////////////////////////////////////////////////
-                int i = 0;
-
-                while (1){  
-                /*while( (UCSR0A & (1<<RXC0))  == 0 ){}; 
-                    br = UDR0;
-                */
-
-                while (( UCSR0A & (1<<UDRE0))  == 0){};
-
-                        for (i = 0; i < strlen(text); i++){ 
-                            while (( UCSR0A & (1<<UDRE0))  == 0){};
-                            UDR0 = text[i]; 
-                            if (i == (strlen(text) - 1)){ 
-                                return 0;
-                            }
-                        } 
-                }
-}
-
-
-
-
 
 int main(void){ 
 				SCH_Init_T1();
@@ -439,66 +398,15 @@ int main(void){
 				
 				
 				ledTest();
-				//SCH_Add_Task(ledRood, 100, 100);
-				//SCH_Add_Task(ledGroen, 200, 100);
-				SCH_Add_Task(read_light_sensor, 100, 500);
-				SCH_Add_Task(read_serial, 5, 100);
+				ledGroen();
+				SCH_Add_Task(read_light_sensor, 100, 100);
+				SCH_Add_Task(read_serial, 200, 100);
 				
 								
 				SCH_Start();
 				
 				while(1){
-				int x;
 				SCH_Dispatch_Tasks();
-				
-				
-				
-				
-				
-				
-				//char str[6];
-				//snprintf(str, 6, "%d", x); 
-				//read_light_sensor();
-				//_delay_ms(10000);
-				
-				
-				
-				/*
-				if(gettemp() >= 26){
-					ledRood();
-				} else if (gettemp() >= 24){
-					ledGeel();
-				} else if (gettemp() >= 20){
-					ledGroen();
-				}*/
-				
-				/*
-				if(read_light_sensor() >= 300){
-					PORTB &= ~_BV(2);
-					PORTB &= ~_BV(3);
-					PORTB |= _BV(1);
-					_delay_ms(10000);
-				} else if (read_light_sensor() >= 100){
-					PORTB &= ~_BV(1);
-					PORTB &= ~_BV(3);
-					PORTB |= _BV(2);
-					_delay_ms(10000);
-				} else if (read_light_sensor() == 0){
-					ledTest();
-				} else if (read_light_sensor() >= 0){
-					PORTB &= ~_BV(1);
-					PORTB &= ~_BV(2);
-					PORTB |= _BV(3);
-					_delay_ms(10000);
-				}	
-				*/				
-				
-				//getdistance();
-				
-				
-				
-				
-				
 				}
 				
 				return 0;
